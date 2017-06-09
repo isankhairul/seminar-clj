dd5022a907d7d3ef9ff7ce791b251d6f91c84eb8
isan <khairul.ikhsan@coderscolony.com>
2017-06-09 23:52:03 +0700

    update history order

---
+++ resources/public/js/module/history-order-seminar.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+
+$('#searchOrder').on('click', function(){
+    
+    var email = $('#email').val();
+    
+    if ( !validateEmail(email) ){
+        alert('Please input emails.');   
+        return false; 
+    }
+
+    $.ajax({
+        type: "POST",
+        dataType: "html",
+        url: context + "/get-history-order",
+        data: {'email': email},
+        cache: false,
+        beforeSend: function() {
+            $('#resultHistoryOrder').empty();
+        },
+        success: function(data){
+            $('#resultHistoryOrder').html( data );
+        },
+        error: function (request, status, error) {
+            //alert(error);
+        },
+        complete: function(){
+        }
+    });
+    
+    console.log(email);
+})
+
+});
--- resources/public/js/module/order-seminar.js
+++ resources/public/js/module/order-seminar.js
@@ -1,8 +1,3 @@
-function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-}
-
 $(document).ready(function(){
 
 $('.btn-order').on('click', function(){
@@ -19,14 +14,15 @@ $('.btn-order').on('click', function(){
 })
 
 $('#submitOrder').on('click', function(){
+    
+    var id = $('#orderSeminarId').val();
+    var email = $('#email').val();
+    
     if ( !validateEmail(email) ){
         alert('Please input emails.');   
         return false; 
     }
 
-    var id = $('#orderSeminarId').val();
-    var email = $('#email').val();
-    
     $.ajax({
         type: "POST",
         dataType: "json",
