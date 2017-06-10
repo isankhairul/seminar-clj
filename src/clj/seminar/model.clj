09cf0408cb8aa85a1939e0cdea2c5c7b7b75c1de
isan <khairul.ikhsan@coderscolony.com>
2017-06-10 10:40:43 +0700

    add model seminar

--- src/clj/seminar/model.clj
+++ src/clj/seminar/model.clj
@@ -1,3 +1,38 @@
 (ns seminar.model
-  (:require [cprop.core :refer [load-config]]))
+  (:require [clojure.tools.logging :as log]
+            [clojure.string :as s]
+            [clojure.java.jdbc :as jdbc]
+            [korma
+             [core :refer :all :as kc]
+             [db :refer :all :as kdb]]
+            [clj-time
+             [core   :as tc]
+             [format :as tf]
+             [coerce :as tcc]
+             [local  :as tl]]))
+
+(def db-jdbc {:classname "com.mysql.jdbc.Driver"
+              :subprotocol "mysql"
+              :subname (str "//127.0.0.1"
+                            "3306"
+                            "/monica")
+              :user "root"
+              :password ""})
+
+(defdb db (mysql
+           {:db "seminar_scraping"
+            :user "root"
+            :password ""
+            :host "127.0.0.1"
+            :port "3306"}))
+
+(defentity seminar
+  (pk :seminarId))
+
+(defentity member
+  (pk :memberId))
+
+(defentity seminar_order
+  (pk :orderId))
+
 
